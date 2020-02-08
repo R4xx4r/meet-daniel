143ea79ec4ed757793ffe3529713fb80c36e4d68
Daniel Murth <daniel.murth@chello.at>
2020-02-08 01:53:18 +0100

    add home button styling

--- src/components/sections/Home.vue
+++ src/components/sections/Home.vue
@@ -8,8 +8,12 @@
         Hallo, Ich heiße <span class="highlight">Daniel Murth</span>
         und bin Web Developer.
       </div>
-      <div class="content__button">
-        Zu meinen Projekten
+      <div class="content__button button">
+        <a class="button__link link" href="#projects">
+          Zu meinen Projekten
+
+          <svg class="icon icon--arrow" xmlns="http://www.w3.org/2000/svg" width="44.952" height="44.952" viewBox="0 0 44.952 44.952"><path fill="currentColor" d="M44.952 22.108c0-1.25-.478-2.424-1.362-3.308L30.627 5.831a2.5 2.5 0 00-3.536 0 2.511 2.511 0 000 3.546l10.574 10.57H2.484C1.102 19.948 0 21.081 0 22.464v.028c0 1.382 1.102 2.523 2.484 2.523h35.182L27.094 35.579a2.504 2.504 0 003.538 3.54l12.958-12.97a4.633 4.633 0 001.362-3.309v-.732z"/></svg>
+        </a>
       </div>
     </div>
 
@@ -60,12 +64,41 @@
   }
 
   .content__text {
-    font-size: 44px;
-    line-height: 50px;
+    font-size: 48px;
+    line-height: 54px;
   }
 
   .content__button {
+    margin-top: 35px;
+  }
 
+  .button__link {
+    position: relative;
+    padding: 12px 30px;
+    width: 250px;
+    margin: 0 auto;
+    border: 2px solid $color-white;
+    background-color: transparent;
+    font-size: 20px;
+    text-align: left;
+    transition: background-color .25s ease-in-out,
+                border .25s ease-in-out;
+
+    &:hover {
+      background-color: $color-new-grass;
+      border-color: $color-new-grass;
+    }
+  }
+
+  .icon--arrow {
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+    transition: transform .35s ease-in-out;
+  }
+  .button__link:hover .icon--arrow {
+    transform: translateY(-50%) rotate(90deg);
   }
 
   .nav--main {
@@ -84,4 +117,5 @@
     justify-content: center;
     height: 100%;
   }
+
 </style>

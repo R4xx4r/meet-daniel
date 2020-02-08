9547adf27a0cb1ed960142575356737b85e8e443
Daniel Murth <daniel.murth@chello.at>
2020-02-08 10:24:49 +0100

    add https://www.npmjs.com/package/vue2-smooth-scroll (smooth scroll) to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueSmoothScroll from 'vue2-smooth-scroll'; // https://www.npmjs.com/package/vue2-smooth-scroll
 
 Vue.config.productionTip = false
+
+Vue.use(vueSmoothScroll)
 
 new Vue({
   render: h => h(App),

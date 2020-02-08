4702f00329c3f03afcc18fdd2e102b89146640cd
Daniel Murth <daniel.murth@chello.at>
2020-02-08 11:53:17 +0100

    add https://vincentgarreau.com/particles.js/ (particlejs) because of header animation

--- src/components/sections/Home.vue
+++ src/components/sections/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="block block--home" id="home">
     
-    <div class="canvas"></div>
+    <div class="canvas" id="particles-js"></div>
 
     <div class="content-wrapper content">
       <div class="content__text">
@@ -38,8 +38,127 @@
 </template>
 
 <script>
+  import 'particles.js';
+
   export default {
-    name: 'md-home'
+    name: 'md-home',
+    mounted() {
+      this.initParticles();
+    },
+    methods: {
+      initParticles() {
+        window.particlesJS("particles-js", {
+          "particles": {
+            "number": {
+              "value": 360,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
+            },
+            "color": {
+              "value": "#ffffff"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              },
+              "image": {
+                "src": "",
+                "width": 170,
+                "height": 50
+              }
+            },
+            "opacity": {
+              "value": 0.6313181133058181,
+              "random": true,
+              "anim": {
+                "enable": true,
+                "speed": 0.8791208791208792,
+                "opacity_min": 0,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 4,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 4,
+                "size_min": 0.3,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": false,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.4,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 1,
+              "direction": "top",
+              "random": true,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+              }
+            }
+          },
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": false,
+                "mode": "repulse"
+              },
+              "onclick": {
+                "enable": false,
+                "mode": "repulse"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 400,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 250,
+                "size": 0,
+                "duration": 2,
+                "opacity": 0,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 400,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+        });
+      }
+    }
   }
 </script>
 

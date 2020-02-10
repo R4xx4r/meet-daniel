1b5e6c03dd84087f8779ee01c3bbc11fc49f9f1c
Daniel Murth <daniel.murth@chello.at>
2020-02-10 13:49:00 +0100

    adapt main js because of vue-svgicon and create folder with the first svgs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vueSmoothScroll from 'vue2-smooth-scroll'; // https://www.npmjs.com/package/vue2-smooth-scroll
+import SvgIcon from 'vue-svgicon'; // https://www.npmjs.com/package/vue-svgicon
 
 Vue.config.productionTip = false
 
 Vue.use(vueSmoothScroll)
+Vue.use(SvgIcon, {
+  tagName: 'svgicon',
+  classPrefix: 'svgicon-'
+});
 
 new Vue({
   render: h => h(App),

b25935bbf60dfc231d5376198c95757e4f7a9474
Daniel Murth <daniel.murth@chello.at>
2020-02-12 22:10:07 +0100

    project optical first draft

--- src/components/partials/Project.vue
+++ src/components/partials/Project.vue
@@ -36,9 +36,25 @@
 
 <style lang="scss" scoped>
   .project {
-    width: 350px;
-    height: 300px;
     position: relative;
+    width: 400px;
+    height: 350px;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: $color-clean;
+      opacity: 0;
+      transition: opacity .5s ease-in-out;
+    }
+
+    &:hover::before {
+      opacity: 1;
+    }
   }
 
   .project__image-wrapper {
@@ -48,12 +64,62 @@
 
   .project__image {
     height: 100%;
-    width: auto;
+    width: 100%;
     object-fit: cover;
+    object-position: top center;
   }
 
   .project__overlay {
-    display: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 100%;
+    opacity: 0;
+    transition: all .3s ease-in-out;
+  }
+
+  .project__overlay--top {
+    top: 0;
+  }
+  .project__overlay--bottom {
+    bottom: 0;
+  }
+  
+  .project:hover .project__overlay--top {
+    top: 75px;
+    opacity: 1;
+  }
+  .project:hover .project__overlay--bottom {
+    bottom: 50px;
+    opacity: 1;
+  }
+
+  .overlay__title {
+    font-size: 20px;
+    color: $color-black;
+    font-weight: $font-weight-bold;
+  }
+  .overlay__subtitle {
+    font-size: 18px;
+    margin-top: 15px;
+    color: $color-new-grass;
+  }
+
+  .overlay__button {
+    cursor: pointer;
+    padding: 15px 30px;
+    color: $color-dark-gray;
+    background-color: transparent;
+    border: 2px solid $color-new-grass;
+    transition: background-color .25s ease-in-out,
+                color .25s ease-in-out;
+
+    &:hover {
+      color: $color-white;
+      background-color: $color-new-grass;
+    }
   }
 
 </style>
